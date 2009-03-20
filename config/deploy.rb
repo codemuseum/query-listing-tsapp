f85756a54bdba7998c550ecb69a82d29ab147fce
Matthew Moore <matt@thrivesmart.com>
2009-03-20 15:18:37 -0600

    using the newest ts rails plugin

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,8 +30,38 @@ namespace :deploy do
     run "mkdir -p #{shared_path}/config" 
     put '', "#{shared_path}/config/database.yml" 
   end
+  
+  desc "Make symlink for backup_fu.yml" 
+  task :symlink_backupfu_yaml do
+    run "ln -nfs #{shared_path}/config/backup_fu.yml #{release_path}/config/backup_fu.yml" 
+  end
+  
+  desc "Create empty backup_fu.yml in shared path" 
+  task :create_backupfu_yaml do
+    run "mkdir -p #{shared_path}/config" 
+    put '', "#{shared_path}/config/backup_fu.yml" 
+  end
+
+
+  desc "Make symlink for tsapp.yml"
+  task :symlink_tsapp_yaml do
+    run "ln -nfs #{shared_path}/config/tsapp.yml #{release_path}/config/tsapp.yml" 
+  end
+  
+  desc "Create empty tsapp.yml in shared path" 
+  task :create_tsapp_yaml do
+    run "mkdir -p #{shared_path}/config" 
+    put '', "#{shared_path}/config/tsapp.yml" 
+  end
 end
 
 after 'deploy:setup', 'deploy:create_dbyaml'
 after 'deploy:update_code', 'deploy:symlink_dbyaml'
+
+after 'deploy:setup', 'deploy:create_backupfu_yaml'
+after 'deploy:update_code', 'deploy:symlink_backupfu_yaml'
+
+after 'deploy:setup', 'deploy:create_tsapp_yaml'
+after 'deploy:update_code', 'deploy:symlink_tsapp_yaml'
+
 after "deploy", "deploy:cleanup"
